2a599cd0115cf65e385ea1791b1b684712512bf1
DMCatGithub <125546632+DMCatGithub@users.noreply.github.com>
2026-05-18 13:39:19 +1200

    test

--- streamlit_allergy_app.py
+++ streamlit_allergy_app.py
@@ -203,8 +203,8 @@ def plane_destinations ():
 
 
 routes_from_df = plane_destinations()
-st.table(routes_from_df)
-# st.table(routes_from_df[["City", "Country", "Distance"]])
+# st.table(routes_from_df)
+st.table(routes_from_df[["City", "Country", "Distance"]])
 
 # *************************
 
